b0ade85fb3d461ea114029e02009ea12845dd248
adham165 <adhamanas2004@gmail.com>
2025-09-16 04:06:56 +0300

    regesteration and fix login

--- backend/Registration/registration.go
+++ backend/Registration/registration.go
@@ -1,6 +1,40 @@
 package Registration
 
 import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
 	"university-management/backend/models"
 )
+func RegisterStudent(path string ) error {
+	file, err := os.OpenFile(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
 
+	reader := csv.NewReader(file)
+	records, err := reader.ReadAll()
+	if err != nil {
+		return err
+	}
+	for i, row := range records {
+		if i == 0 {
+			continue
+		}
+
+		age, err := strconv.Atoi(row[1])
+		if err != nil {
+			fmt.Println("Error converting age:", err)
+			continue
+		}
+
+		studen := models.Student{
+			Name:  row[0],
+			Age:   age,
+			Email: row[2],
+		}
+	}
+
+}
